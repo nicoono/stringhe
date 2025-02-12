8c0764eaa3014d656dad2983a3274cd03b7d7316
nicoono <onorati.nicolo@franchettisalviani.net>
2025-02-12 08:34:52 +0100

    terza versione
    Questo è lo svolgimento del terzo esercizio

--- Program.cs
+++ Program.cs
@@ -10,9 +10,28 @@
     return true;
 }
 
+
+void spezzaMeta(string stringa)
+{
+    Console.Write("Inserisci una stringa: ");
+    string input = Console.ReadLine();
+
+    int metà = input.Length / 2;
+
+    string primaParte = input.Substring(0, metà);
+    string secondaParte = input.Substring(metà);
+
+    Console.WriteLine(primaParte);
+    Console.WriteLine(secondaParte);
+}
+
+
 // MAIN 
 // ESERCIZIO 4
 string stringa = "";
 Console.WriteLine("dimmi una parola");
 stringa = Console.ReadLine();
 Console.WriteLine(palindroma(stringa));
+
+// ESERCIZIO 3
+spezzaMeta(stringa);
