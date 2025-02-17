509f3776ac8e28c041b42ef739c3bba766d25e44
nicoono <onorati.nicolo@franchettisalviani.net>
2025-02-17 13:25:40 +0100

    versione finale
    questa è lo svolgimento dell'esercizio C simile a quello sulla verifica

---
+++ eseC/eseC/Program.cs
@@ -0,0 +1,26 @@
+﻿
+bool controlla_parentesi(string esp)
+{
+    int aperte = 0, chiuse = 0;
+    for (int i = 0; i < esp.Length; i++)
+    {
+        if (esp[i] == '(')
+        {
+            aperte++;
+        }
+        else if (esp[i] == ')')
+        {
+            chiuse++;
+        }
+
+        if (chiuse > aperte)
+        {
+            return false;
+        }
+    }
+    if (aperte == chiuse)
+    {
+        return true;
+    }
+    return false;
+}
